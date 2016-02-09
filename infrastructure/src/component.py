e7d349abb1c072422badb112d716243bc91044fb
Hartnell Foster <hartnell.foster@bbc.co.uk>
2016-02-09 15:42:20 +0000

    Set healthcheck URL

--- infrastructure/src/component.py
+++ infrastructure/src/component.py
@@ -1,4 +1,5 @@
 from BBC.AWS.CloudFormation.Common.Component import Component
 
-component = Component("component-name")
+component = Component("fosteh12-cosmos-training")
+component.set_health_check_url("/status")
 component.render()
